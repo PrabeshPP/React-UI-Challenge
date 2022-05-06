4aacdffe6483f51dc1616a6c504b6be974e2736f
Prabesh <prabesh.bista369@gmail.com>
2022-05-06 23:28:45 +0545

    completed the UI Challenge

--- src/component/Body/Body.js
+++ src/component/Body/Body.js
@@ -1,6 +1,9 @@
 import classes from "./Body.module.css";
-import {BsArrowUpRight} from "react-icons/bs";
+import {BsArrowUpRight,BsArrowRight,BsArrowUp} from "react-icons/bs";
 import {CgProfile} from "react-icons/cg";
+import {FaBitcoin} from "react-icons/fa";
+import {IoMdAdd} from "react-icons/io";
+
 
 const Body = () => {
   return (
@@ -15,7 +18,7 @@ const Body = () => {
           <span>,fast</span> and <span>secure.</span>
         </p>
       </div>
-    <dv className={classes.boxButton}>
+    <div className={classes.boxButton}>
         <div className={classes.button1}>
         {<BsArrowUpRight/>}
         <p>Get Started</p>
@@ -24,13 +27,52 @@ const Body = () => {
         {<BsArrowUpRight/>}
         <p>Contact Sales</p>
         </div>
-    </dv>
-
-    <div>
-        <div>
-
-        </div>
     </div>
+
+    <div className={classes.footerBox11}>
+    
+    <div className={classes["box-comment"]}>
+        <div className={classes.box111}>
+        {<CgProfile className={classes.person}/>}
+        <div className={classes.comment}>
+            <h3>Prabesh Bista</h3>
+            <p><span>"</span>one of the best services I have experiece.<span>"</span></p>
+        </div>
+        </div>
+       
+            {<BsArrowRight className={classes.arrowRight}/>}
+        
+    </div>
+    <div className={classes["box-comment"]}>
+        <div className={classes.box222}>
+        {<FaBitcoin className={classes.bitcoin}/>}
+        <div className={classes.btc}>
+            <p>BTC/USDT</p>
+            <p>$36049.80</p>
+        </div>
+        <div className={classes.verticalLine}/>
+        <div className={classes.change}>
+            <div className={classes.change11}>
+            <BsArrowUp/>
+            <span>20.22%</span></div>
+            <div className={classes.change11}>
+            <IoMdAdd/>
+            <span>$3000.69</span></div>
+            
+        </div>
+
+        
+        </div>
+       
+            {<BsArrowRight className={classes.arrowRight}/>}
+        
+    </div>
+    
+    </div>
+
+    
+   
+
 
     </div>
   );
